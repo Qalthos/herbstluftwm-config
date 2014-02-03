bee4a7766547768656c2ae4a1fea2b5bd4cfca88
Nathaniel Case <Qalthos@gmail.com>
2014-02-03 13:49:59 -0500

    Pad monitors individually

--- panel2.sh
+++ panel2.sh
@@ -2,11 +2,14 @@
 # vim: set fileencoding=utf-8 ts=4 sts=4 sw=4 tw=80 expandtab :
 # Florian Bruhin <me@the-compiler.org>
 
+monitor=$1
+
 [[ $0 == /* ]] && script="$0" || script="${PWD}/${0#./}"
 panelfolder=${script%/*}
 trap 'herbstclient emit_hook quit_panel' TERM
-herbstclient pad 0 16
+herbstclient pad $monitor 16
 herbstclient emit_hook quit_panel
+
 
 dzen_fg="#d0d0d0"
 dzen_bg="#1c1c1c"
